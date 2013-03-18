a2269017f89d59f9ad73bcfc42fb09b3e24ce19c
Untou4able <gg.russia.crew@gmail.com>
2013-03-18 13:54:07 +0800

    implemented ont_rg

--- eltex_ntp.php
+++ eltex_ntp.php
@@ -3,7 +3,7 @@ include_once 'snmp_eltex.php';
 include_once 'iSnmp_eltex.php';
 
 class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
-    protected $mac = '', $eltex_options, $slot;
+    protected $mac = '', $eltex_options, $slot, $rg = FALSE, $rg_oids = Array();
     
     protected $del_oids = Array(
         'pon' => Array(
@@ -33,26 +33,25 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
         ),
         'acs' => Array(
             'acs_service_prof' => '1.3.6.1.4.1.35265.1.22.3.15.1.3.8.69.76.84.88%deced_mac% s %value%'
-        )
-        /*
+        ),
         'ntp_rg' => Array(
-            'ppp' => Array(
-                'ppp_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.11.8.69.76.84.88%deced_mac% s %value%',
-                'ppp_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.12.8.69.76.84.88%deced_mac% s %value%'
-            ),
-            'voip1' => Array(
-                'sip_s' => '1.3.6.1.4.1.35265.1.22.3.15.1.10.8.69.76.84.88%deced_mac% s %value%',
-                'voip1_e' => '1.3.6.1.4.1.35265.1.22.3.15.1.4.8.69.76.84.88%deced_mac% s %value%',
-                'viop1_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.5.8.69.76.84.88%deced_mac% s %value%',
-                'voip1_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.6.8.69.76.84.88%deced_mac% s %value%'
-            ),
-            'voip2' => Array(
-                'voi2_e' => '1.3.6.1.4.1.35265.1.22.3.15.1.7.8.69.76.84.88%deced_mac% s %value%',
-                'voip2_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.8.8.69.76.84.88%deced_mac% s %value%',
-                'voip2_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.9.8.69.76.84.88%deced_mac% s %value%'
+            'ppp_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.11.8.69.76.84.88%deced_mac% s %value%',
+            'ppp_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.12.8.69.76.84.88%deced_mac% s %value%',
+            'sip_s' => '1.3.6.1.4.1.35265.1.22.3.15.1.10.8.69.76.84.88%deced_mac% s 172.31.254.34',
+            'voip1_e' => '1.3.6.1.4.1.35265.1.22.3.15.1.4.8.69.76.84.88%deced_mac% s Enable',
+            'voip1_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.5.8.69.76.84.88%deced_mac% s %value%',
+            'voip1_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.6.8.69.76.84.88%deced_mac% s %value%',
+            'voip2_e' => '1.3.6.1.4.1.35265.1.22.3.15.1.7.8.69.76.84.88%deced_mac% s Enable',
+            'voip2_l' => '1.3.6.1.4.1.35265.1.22.3.15.1.8.8.69.76.84.88%deced_mac% s %value%',
+            'voip2_p' => '1.3.6.1.4.1.35265.1.22.3.15.1.9.8.69.76.84.88%deced_mac% s %value%',
+            'inner_vlan' => '1.3.6.1.4.1.35265.1.22.3.5.1.5.2.%slot%.8.69.76.84.88%deced_mac% u %value%',
+            'vpn_e' => '1.3.6.1.4.1.35265.1.22.3.5.1.4.2.%slot%.8.69.76.84.88%deced_mac% i 1',
+            'vpn_cfg' => Array(
+                '1.3.6.1.4.1.35265.1.22.3.60.1.5.8.69.76.84.88%deced_mac%.65536 i 4',
+                '1.3.6.1.4.1.35265.1.22.3.60.1.3.8.69.76.84.88%deced_mac%.65536 s InternetGatewayDevice.WANDevice.5.WANConnectionDevice.2.WANIPConnection.1.X_BROADCOM_COM_VlanMuxID',
+                '1.3.6.1.4.1.35265.1.22.3.60.1.4.8.69.76.84.88%deced_mac%.65536 s %value%'
             )
         )
-        */
     );
     
     public function __construct($ip, $slot = 1, $options = NULL) {
@@ -67,6 +66,37 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
         parent::__construct($ip, $this->eltex_options);
     }
     
+    private function add_rg_mac() {
+        if(array_key_exists('voip1_l', $this->rg_oids)) {
+            $this->rg_oids['sip_s'] = $this->oids['ntp_rg']['sip_s'];
+            $this->rg_oids['voip1_e'] = $this->oids['ntp_rg']['voip1_e'];
+        }
+        if(array_key_exists('voip2_l', $this->rg_oids)) {
+            $this->rg_oids['voip2_e'] = $this->oids['ntp_rg']['voip2_e'];
+        }
+        if(array_key_exists('inner_vlan', $this->rg_oids)) {
+            $this->rg_oids['vpn_e'] = $this->oids['ntp_rg']['vpn_e'];
+            $this->oids['ntp_rg']['vpn_cfg'] = preg_replace('/%deced_mac%/', $this->mac, $this->oids['ntp_rg']['vpn_cfg']);
+        }
+        $this->rg_oids = preg_replace('/%deced_mac%/', $this->mac, $this->rg_oids);
+    }
+    
+    private function add_rg() {
+        foreach($this->rg_oids as $oid) {
+            $this->object_id = $oid;
+            $this->set_ont();
+            $this->send();
+        }
+        if(array_key_exists('inner_vlan', $this->rg_oids)) {
+            $this->object_id = '';
+            foreach($this->oids['ntp_rg']['vpn_cfg'] as $oid) {
+                $this->object_id .= $oid.' ';
+            }
+            $this->set_ont();
+            $this->send();
+        }
+    }
+
     private function bunch_sender() {
         $snmp_send_counter = 0;
         do {
@@ -76,6 +106,7 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
         if($snmp_send_counter >= parent::MAX_SNMP_SEND_TRIES )
             $this->snmp_return = '2';
     }
+    
     private function create_acs($mac) {
         $this->set($this->create_oids['acs'], 's', $mac);
         $this->send();
@@ -93,6 +124,9 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
     private function add_slot() {
         $this->oids['pon'] = preg_replace('/%slot%/', $this->slot, $this->oids['pon']);
         $this->create_oids['pon'] = preg_replace('/%slot%/', $this->slot, $this->create_oids['pon']);
+        if($this->rg && !empty($this->rg_oids)) {
+            $this->rg_oids = preg_replace('/%slot%/', $this->slot, $this->rg_oids);
+        }
     }
     
     private function del_slot() {
@@ -110,6 +144,9 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
         $this->oids['pon'] = preg_replace('/%deced_mac%/', $this->mac, $this->oids['pon']);
         $this->oids['acs'] = preg_replace('/%deced_mac%/', $this->mac, $this->oids['acs']);
         $this->create_oids = preg_replace('/%deced_mac%/', $this->mac, $this->create_oids);
+        if($this->rg && !empty($this->rg_oids)) {
+            $this->add_rg_mac();
+        }
     }
     
     protected function mac_to_oid($mac) {
@@ -140,6 +177,9 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
             $this->bunch_sender();
             if($this->snmp_return == '0')
                 $this->save_ont();
+        }
+        if($this->rg) {
+            $this->add_rg();
         }
         return $this->snmp_return;
     }
@@ -174,6 +214,12 @@ class eltex_ntp extends snmp_eltex implements iSnmp_eltex {
             $this->oids['pon'][$prop] = preg_replace('/%value%/', $value, $this->oids['pon'][$prop]);
         } elseif (array_key_exists($prop, $this->oids['acs'])) {
             $this->oids['acs'][$prop] = preg_replace('/%value%/', $value, $this->oids['acs'][$prop]);
+        } elseif(array_key_exists($prop, $this->oids['ntp_rg']) && !empty($value)) {
+            $this->rg = TRUE;
+            $this->rg_oids[$prop] = preg_replace('/%value%/', $value, $this->oids['ntp_rg'][$prop]);
+            if($prop == 'inner_vlan') {
+                $this->oids['ntp_rg']['vpn_cfg'][2] = preg_replace('/%value%/', $value, $this->oids['ntp_rg']['vpn_cfg'][2]);
+            }
         }
     }
 }
